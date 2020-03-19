eac789682203dc1e2d6e7a78afda9d82784168bb
Karthick0298 <46165891+Karthick0298@users.noreply.github.com>
2020-03-19 11:00:09 +0530

    Updates

---
+++ precache-manifest.1010f0faa251fb5ee31f471bc7279c47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0210c374a9703d35103c1a0123b51f",
+    "url": "/Karthick0298/cri/index.html"
+  },
+  {
+    "revision": "784ece6b514bdd598744",
+    "url": "/Karthick0298/cri/static/css/main.f40db810.chunk.css"
+  },
+  {
+    "revision": "bc3ac24aa6d426f428fc",
+    "url": "/Karthick0298/cri/static/js/2.9d8d73a4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Karthick0298/cri/static/js/2.9d8d73a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784ece6b514bdd598744",
+    "url": "/Karthick0298/cri/static/js/main.e4fea507.chunk.js"
+  },
+  {
+    "revision": "06a30221ba788e8da3f5",
+    "url": "/Karthick0298/cri/static/js/runtime-main.72bdc3a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Karthick0298/cri/static/media/logo.5d5d9eef.svg"
+  }
+]);
