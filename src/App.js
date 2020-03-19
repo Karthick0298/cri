001cee0f18fa54d7dd31074414c6cad0fc1219ca
Karthick0298 <46165891+Karthick0298@users.noreply.github.com>
2020-03-19 10:29:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+class App extends Component {
+  render(){
+    return (
+		<div class="full-container">
+
+
+		<header>
+
+			<img src="asserts/logo.png"/>
+			
+		</header>
+
+
+		<div class="container">
+
+
+			<div class="shield">
+
+				<img src="asserts/1.png"/>
+				
+			</div>
+
+			<div class="content">
+
+				<div class="details">
+					<h2>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</h2>
+
+					<ul>
+						<li>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
+						<li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
+					</ul>					
+				</div>
+				<div class="banner">
+
+					<img src="asserts/2.png"/>
+					<p>Government of India has awarded the <b>"National Energy Conservation Award 2018".</b> Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
+				</div>
+			</div>
+			<div class="models">
+
+				<h4>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</h4>
+
+				<img src="asserts/3.png"/>
+
+				<h3>Valves - Pumps - Pipes - IoT Drives {"&"} Controllers - Wires {"&"} Cables - Solar Systems - Motors </h3>	
+			</div>
+			<div class="links">
+
+				<h4>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h4>
+
+				<h4>
+				CHEMICALS {"&"} PROCESS <font color="red"> | </font> POWER <font color="red"> | </font>WATER {"&"} WASTE WATER <font color="red"> | </font> OILS {"&"} GAS <font color="red"> | </font> PHARMA 
+	<font color="red"> | </font> SUGARS {"&"} DISTILLERIES <font color="red"> | </font> PAPER {"&"} PULP
+	<font color="red"> | </font> MARINE {"&"} DEFENCE <font color="red"> | </font> METAL {"&"}MINING <font color="red"> | </font> FOOD {"&"} BEVERAGE <font color="red"> | </font>PETROCHEMICAL {"&"} REFINERIES 
+	<font color="red"> | </font>SOLAR <font color="red"> | </font>BUILDING <font color="red"> | </font> HVAC <font color="red"> | </font> FIRE FIGHTING <font color="red"> | </font> AGRICULTURE {"&"} RESIDENTIAL
+	</h4>
+
+			</div>			
+
+			
+<div class="grid-container">
+  <div class="grid-item" class="fa fa-phone" align= "center">Toll free 1800 200 1234</div>
+  <div class="grid-item" class="fa fa-facebook-square" align = "center">www.facebook.com/cripumps</div>
+  <div class="grid-item" class="fas fa-globe" align = "center">www.crigroups.com</div>  
+  
+</div>
+
+		</div>
+	</div>
+	
   );
 }
-
+}
 export default App;
